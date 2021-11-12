ecf1c70847dbd664eef3336a56df541618831f6c
Phillip Look <phillip.look@brainbits.net>
2021-11-12 16:51:30 +0100

    feat: Support snapshot testing

--- README.md
+++ README.md
@@ -297,3 +297,60 @@ public function testRequest(): void
 }
 
 ```
+
+## Snapshot Trait
+
+To use snapshot tests, use the `SnapshotTrait` in your test class.
+
+Optionally modify the default path of the `__snapshot__` directories appropriate to your
+PSR-4 autoload configuration in `composer.json` by overwriting the `snapshotPath()` method.
+
+```php
+// FunctionalTestCase.php
+
+use Brainbits\FunctionalTestHelpers\Snapshot\SnapshotTrait;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+abstract class FunctionalTestCase extends WebTestCase
+{
+    use SnapshotTrait;
+
+    /**
+     * Overwrite Snapshot::snapshotPath() to locate __snapshot__ directory next to the test classes.
+     */
+    private function snapshotPath(): string
+    {
+        // parent directory is the base path of all tests    
+        $basePath = dirname(__DIR__);
+
+        // convert namespace to path
+        $path = preg_replace('/\\\\[^\\\\]+$/', '', static::class);
+        $path = str_replace('\\', '/', $path);
+        
+        // strip 'App/Tests' from path
+        $path = substr($path, strlen('App/Tests/'));
+
+        return $basePath . '/' . $path;
+    }
+}
+```
+
+Call assertions to verify json with snapshot. The snapshot file is created automatically on the first run.
+
+```php
+public function testJsonFactoryCreatesJsonCorrectly()
+{
+    $jsonFactory = new JsonFactory();
+
+    $json = $jsonFactory->createJson();
+
+    $this->assertMatchesJsonSnapshot($json);
+}
+```
+
+To recreate/update snapshot files run PHPUnit with the environment variable `UPDATE_SNAPSHOTS`.
+
+```shell
+UPDATE_SNAPSHOTS=1 phpunit
+UPDATE_SNAPSHOTS=1 phpunit tests/Functinoal/MyTest.php
+```
---
+++ src/Snapshot/SnapshotTrait.php
@@ -0,0 +1,154 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brainbits\FunctionalTestHelpers\Snapshot;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Filesystem\Filesystem;
+
+use function dirname;
+use function getenv;
+use function mb_strtolower;
+use function rtrim;
+use function Safe\json_decode;
+use function Safe\json_encode;
+use function Safe\preg_match;
+use function Safe\preg_replace;
+use function Safe\sprintf;
+use function Safe\substr;
+use function str_replace;
+use function strlen;
+use function strpos;
+
+use const JSON_PRETTY_PRINT;
+use const JSON_UNESCAPED_LINE_TERMINATORS;
+use const JSON_UNESCAPED_SLASHES;
+use const JSON_UNESCAPED_UNICODE;
+use const PHP_EOL;
+
+/**
+ * @mixin TestCase
+ */
+trait SnapshotTrait
+{
+    /** @var array<string,int> */
+    private array $filenames = [];
+
+    /**
+     * @param mixed[] $actual
+     */
+    final protected function assertMatchesArraySnapshot(array $actual, string $message = ''): void
+    {
+        $this->assertMatchesJsonSnapshot(json_encode($actual), $message);
+    }
+
+    final protected function assertMatchesJsonSnapshot(string $actual, string $message = ''): void
+    {
+        self::assertJson($actual, $message);
+
+        $fixtureFilename = $this->snapshotFilename();
+
+        $this->snapshotDumpJson($fixtureFilename, $actual);
+
+        self::assertJsonStringEqualsJsonFile(
+            $fixtureFilename,
+            $actual,
+            $this->snapshotAppendFilenameHint($fixtureFilename, $message)
+        );
+    }
+
+    private function snapshotFilename(): string
+    {
+        $filename = sprintf(
+            '%s/__snapshots__/%s.json',
+            $this->snapshotPath(),
+            $this->snapshotName()
+        );
+
+        if (!($this->filenames[$filename] ?? false)) {
+            $this->filenames[$filename] = 1;
+
+            return $filename;
+        }
+
+        return sprintf(
+            '%s/__snapshots__/%s_%s.json',
+            $this->snapshotPath(),
+            $this->snapshotName(),
+            ++$this->filenames[$filename],
+        );
+    }
+
+    /**
+     * Overwrite function in using class to locate __snapshot__ directory correctly.
+     */
+    private function snapshotPath(): string
+    {
+        $path = preg_replace('/\\\\[^\\\\]+$/', '', static::class);
+        $path = str_replace('\\', '/', $path);
+
+        return $path;
+    }
+
+    private function snapshotName(): string
+    {
+        $class = preg_replace('/.*\\\\/', '', static::class);
+
+        if (substr($class, -4) === 'Test') {
+            $class = substr($class, 0, -4);
+        }
+
+        $class = preg_replace('/(.)([[:upper:]])/u', '\1_\2', $class);
+        $class = mb_strtolower($class);
+
+        $method = $this->getName(false);
+        if (strpos($method, 'test') === 0) {
+            $method = substr($method, strlen('test'));
+        }
+
+        $method = preg_replace('/(.)([[:upper:]])/u', '\1_\2', $method);
+        $method = mb_strtolower($method);
+
+        $dataset = mb_strtolower($this->getDataSetAsString(false));
+        $matches = [];
+        preg_match('/"([^"]+)"/', $dataset, $matches);
+
+        $dataset = $matches[1] ?? $dataset;
+
+        $lowercase = mb_strtolower($class . ':' . $method . ':' . $dataset);
+        $noSpecialCharacters = preg_replace('/[^\pL0-9]+/u', '_', $lowercase);
+
+        return rtrim($noSpecialCharacters, '_');
+    }
+
+    private function snapshotDumpJson(string $fixtureFilename, string $string): void
+    {
+        $filesystem = new Filesystem();
+
+        if (!getenv('UPDATE_SNAPSHOTS') && $filesystem->exists($fixtureFilename)) {
+            return;
+        }
+
+        $filesystem->mkdir(dirname($fixtureFilename));
+        $filesystem->dumpFile(
+            $fixtureFilename,
+            json_encode(
+                json_decode($string),
+                JSON_UNESCAPED_SLASHES |
+                JSON_UNESCAPED_LINE_TERMINATORS |
+                JSON_UNESCAPED_UNICODE |
+                JSON_PRETTY_PRINT
+            )
+        );
+    }
+
+    private function snapshotAppendFilenameHint(string $fixtureFilename, string $message): string
+    {
+        $filenameHint = 'Snapshot: ' . $fixtureFilename;
+
+        return $message
+            ? $message . PHP_EOL . $filenameHint
+            : $filenameHint;
+    }
+}
